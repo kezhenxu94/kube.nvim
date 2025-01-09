15550f105459296e3e3150d582cd1ea84aff104d
kezhenxu94 <kezhenxu94@apache.org>
2025-01-09 08:38:24 +0800

    refactor: extract shutdown jobs

--- lua/kube/autocmds.lua
+++ lua/kube/autocmds.lua
@@ -8,15 +8,31 @@ local M = {}
 
 M.augroup = augroup("kube_autocmds", { clear = true })
 
+local shutdown_jobs = function(buf_nr)
+  local buf = _G.kube_buffers[buf_nr]
+  log.debug("shutting down jobs")
+
+  if buf then
+    for job_id, _ in pairs(buf.jobs) do
+      log.debug("shutting down job", job_id)
+      vim.loop.kill(job_id, vim.loop.constants.SIGTERM)
+      buf.jobs[job_id] = nil
+    end
+  end
+
+  _G.kube_buffers[buf_nr] = nil
+end
+
 function M.setup()
   autocmd({ "BufReadCmd" }, {
     group = "kube_autocmds",
     pattern = "kube://*",
     callback = function(ev)
-      local buf_name = vim.api.nvim_buf_get_name(ev.buf)
+      local buf_nr = ev.buf
+      local buf_name = vim.api.nvim_buf_get_name(buf_nr)
       log.debug("loading buffer", buf_name)
 
-      KubeBuffer:new(ev.buf):load()
+      KubeBuffer:new(buf_nr):load()
     end,
   })
 
@@ -24,21 +40,11 @@ function M.setup()
     group = "kube_autocmds",
     pattern = "kube://*",
     callback = function(ev)
-      local buf_name = vim.api.nvim_buf_get_name(ev.buf)
+      local buf_nr = ev.buf
+      local buf_name = vim.api.nvim_buf_get_name(buf_nr)
       log.debug("deleting buffer", buf_name, ev.buf)
 
-      local buf = _G.kube_buffers[ev.buf]
-      log.debug("shutting down jobs")
-
-      if buf then
-        for job_id, _ in pairs(buf.jobs) do
-          log.debug("shutting down job", job_id)
-          vim.loop.kill(job_id, vim.loop.constants.SIGTERM)
-          buf.jobs[job_id] = nil
-        end
-      end
-
-      _G.kube_buffers[ev.buf] = nil
+      shutdown_jobs(buf_nr)
     end,
   })
 end
