5c4af1c4b13144887eb0aa0b39608e18fa19e6b4
kezhenxu94 <kezhenxu94@apache.org>
2025-01-17 20:27:56 +0800

    feat: add more drill down

--- lua/kube/actions/service.lua
+++ lua/kube/actions/service.lua
@@ -2,6 +2,35 @@ local log = require("kube.log")
 
 ---@type Actions
 local M = {
+  drill_down_resource = function(resource, parent)
+    log.debug("drilling down to service", resource)
+
+    local kind = resource.kind:lower()
+    local name = resource.metadata.name
+    local namespace = resource.metadata.namespace
+    local selector = resource.spec.selector
+
+    local buf_name
+    local params = {}
+
+    if namespace then
+      buf_name = string.format("kube://namespaces/%s/pods", namespace)
+    else
+      buf_name = string.format("kube://pods")
+    end
+
+    local selector_params = {}
+    for key, value in pairs(selector) do
+      table.insert(selector_params, string.format("%s=%s", key, value))
+    end
+
+    if #selector_params > 0 then
+      buf_name = buf_name .. "?selector=" .. table.concat(selector_params, ",")
+    end
+
+    vim.cmd.edit(buf_name)
+  end,
+
   forward_port = function(resource, parent)
     log.debug("forwarding port for service", resource, "in namespace", resource.metadata.namespace)
 
--- lua/kube/actions/statefulset.lua
+++ lua/kube/actions/statefulset.lua
@@ -2,6 +2,35 @@ local log = require("kube.log")
 
 ---@type Actions
 local M = {
+  drill_down_resource = function(resource, parent)
+    log.debug("drilling down to statefulset", resource)
+
+    local kind = resource.kind:lower()
+    local name = resource.metadata.name
+    local namespace = resource.metadata.namespace
+    local selector = resource.spec.selector.matchLabels
+
+    local buf_name
+    local params = {}
+
+    if namespace then
+      buf_name = string.format("kube://namespaces/%s/pods", namespace)
+    else
+      buf_name = string.format("kube://pods")
+    end
+
+    local selector_params = {}
+    for key, value in pairs(selector) do
+      table.insert(selector_params, string.format("%s=%s", key, value))
+    end
+
+    if #selector_params > 0 then
+      buf_name = buf_name .. "?selector=" .. table.concat(selector_params, ",")
+    end
+
+    vim.cmd.edit(buf_name)
+  end,
+
   set_image = function(kbuf, resource, parent)
     log.debug("setting image for statefulset", resource.metadata.name, "in namespace", resource.metadata.namespace)
 
